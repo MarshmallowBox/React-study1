fe124a9535915033044cff03acb83eefece22bb4
MarshmallowBox <48012363+MarshmallowBox@users.noreply.github.com>
2021-01-02 21:17:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>asdf</h3>
+      <h4>asdf</h4>
+      <h5>asdf</h5>
+      <h6>asdf</h6>
+    </div>
+  );
+}
